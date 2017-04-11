5286ae28a0456aec25f5f5834d731d2334a6ac3f
jay freeman <jaythinkshappiness@gmail.com>
2017-04-11 15:53:19 -0700

    add backend for customer edit

--- src/app/customer-edit/customer-edit.component.ts
+++ src/app/customer-edit/customer-edit.component.ts
@@ -15,6 +15,9 @@ import { Customer } from '../customer.model';
 export class CustomerEditComponent implements OnInit {
   customerId: string = null;
   customer;
+  edit = new Customer();
+  editValidationMessage = '';
+
 
   constructor(
     private router: Router,
@@ -30,6 +33,19 @@ export class CustomerEditComponent implements OnInit {
         console.log(this.customer);
       });
     });
+  }
+
+  updateCustomer(){
+    this.editValidationMessage = this.edit.validationMessage();
+    if(!this.editValidationMessage){
+      const promise = this.dataService.updateDeliverer(this.edit);
+      promise.then((success) => {
+        // add id to this route to show specific delivery page
+        this.router.navigate(['customer-overview/']);
+      }).catch((failure) => {
+        console.log('Customer save failed');
+      });
+    }
   }
 
 }
