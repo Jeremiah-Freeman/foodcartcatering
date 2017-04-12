98d0a7457f87708442112bb94c526e6435ab588d
David Quisenberry <dmqpdx@gmail.com>
2017-04-12 10:10:54 -0700

    add first round of crud for menu items

--- src/app/edit-menu/edit-menu.component.ts
+++ src/app/edit-menu/edit-menu.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MenuItem } from './../menu-item.model';
+import { DataService } from './../data.service';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-edit-menu',
   templateUrl: './edit-menu.component.html',
-  styleUrls: ['./edit-menu.component.css']
+  styleUrls: ['./edit-menu.component.css'],
+  providers: [DataService]
 })
 export class EditMenuComponent implements OnInit {
 
-  constructor() { }
+  @Input() foodCartId: string;
+  public menuItems: FirebaseListObservable<any[]>;
+  newForm = true;
+  editForm = false;
+  deleteForm = false;
+  edit = new MenuItem();
+  editValidationMessage = '';
+
+  constructor(private router: Router, public dataService: DataService) { }
 
   ngOnInit() {
+    this.menuItems = this.dataService.getMenuItemsByFoodCartId(this.foodCartId);
   }
+
+  resetForm(setNew = true) {
+    this.edit = new MenuItem();
+    this.newForm = false;
+    this.deleteForm = false;
+    this.editForm = false;
+    if (setNew) {
+      this.newForm = true;
+    }
+  }
+
+  saveNewMenuItem() {
+    this.editValidationMessage = this.edit.validationMessage();
+
+    if (!this.editValidationMessage) {
+      this.edit.foodCartID = this.foodCartId;
+      const promise = this.dataService.addMenuItem(this.edit);
+      promise.then((success) => {
+        this.resetForm();
+      }).catch((failure) => {
+        console.log('Menu Item Save New Failed!');
+        console.log(failure);
+      });
+    }
+  }
+
+  deleteMenuItem( menuItem ) {
+    this.resetForm(false);
+    this.edit = menuItem;
+    this.deleteForm = true;
+  }
+
+  deleteMenuItemConfirmed() {
+    const promise = this.dataService.deleteMenuItem(this.edit);
+    promise.then((success) => {
+      this.resetForm();
+    }).catch((failure) => {
+      console.log('MenuItem Delete Failed!');
+      console.log(failure);
+    });
+  }
+
+  editMenuItem(menuItem) {
+    this.resetForm(false);
+    this.edit.copyFields(menuItem);
+    this.editForm = true;
+  }
+
+  updateMenuItem() {
+    this.editValidationMessage = this.edit.validationMessage();
+
+    if (!this.editValidationMessage) {
+      const promise = this.dataService.updateMenuItem(this.edit);
+      promise.then((success) => {
+        this.resetForm();
+      }).catch((failure) => {
+        console.log('Menu Item Update Failed!');
+        console.log(failure);
+      });
+    }
+  }
+
 
 }
