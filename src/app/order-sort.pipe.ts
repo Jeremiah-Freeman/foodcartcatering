7b400025f75a2305d8905f1ec53d8fde638e683e
Benjamin T. Seaver and Clayton C. Collins <benjamin@>
2017-04-12 10:32:42 -0700

    fix date pipe

--- src/app/order-sort.pipe.ts
+++ src/app/order-sort.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Order }
+import { Order } from './order.model'
 
 @Pipe({
   name: 'orderSort',
@@ -7,16 +7,16 @@ import { Order }
 })
 export class OrderSortPipe implements PipeTransform {
 
-  transform(input: Order[], sortOn = 'requestedDeliveryTime'): Order[] {
+  transform(input: Order[], sortOn = 'requestedDeliveryTime') {
     if (input) {
-      const output: Order[];
+      const output: Order[] = [];
 
       if (sortOn === 'requestedDeliveryTime') {
         for (var i = 0; i < input.length; i++) {
           for (var j = 0; j < output.length; j++) {
             if (j >= output.length) {
               output.push(input[i]);
-            } else if (input[i].[sortOn] < output[j].[sortOn]) {
+            } else if (input[i][sortOn] < output[j][sortOn]) {
               output.splice(j, 0, input[i]);
             }
           }
