08ceaa91434011c4a76ceb1a8532cbc775f4b25a
Benjamin T. Seaver and Clayton C. Collins <clayton@>
2017-04-13 10:52:41 -0700

    display foodcart orders until picked up

--- src/app/order-complete.pipe.ts
+++ src/app/order-complete.pipe.ts
@@ -7,12 +7,22 @@ import { Order } from './order.model';
 })
 export class OrderCompletePipe implements PipeTransform {
 
-  transform(input) {
+  transform(input, sortOn) {
     if (input) {
+      console.log(sortOn);
       const output: any[] = [];
-      for (var i = 0; i < input.length; i++) {
-        if (!input[i].order.productionCompletionStatus) {
-          output.push(input[i])
+      if (sortOn === 'incomplete'){
+
+        for (var i = 0; i < input.length; i++) {
+          if (!input[i].order.productionCompletionStatus) {
+            output.push(input[i])
+          }
+        }
+      } else if (sortOn === 'complete') {
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].order.productionCompletionStatus) {
+            output.push(input[i])
+          }
         }
       }
       return output;
