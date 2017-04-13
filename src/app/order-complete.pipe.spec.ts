66fbcddf39f298b40869e7af0fff54e56e9f06e5
Benjamin T. Seaver and Clayton C. Collins <benjamin@>
2017-04-13 10:21:31 -0700

    add order complete pipe

---
+++ src/app/order-complete.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { OrderCompletePipe } from './order-complete.pipe';
+
+describe('OrderCompletePipe', () => {
+  it('create an instance', () => {
+    const pipe = new OrderCompletePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
